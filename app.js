f81ed79d0181ff20182ea8b998da356afb3bde86
Atsighi Bright <brytebee@gmail.com>
2022-10-07 14:23:46 +0100

    :white_check_mark: Update app.js
    Install yags and write  add, remove, read and list functions

--- app.js
+++ app.js
@@ -1,2 +1,48 @@
-const myNotes = require("./notes");
-console.log(myNotes());
+// const validator = require("validator");
+// const myNotes = require("./notes");
+// const chalk = require("chalk");
+const { string } = require("yargs");
+const yargs = require("yargs");
+yargs.version("1.1.0");
+
+yargs.command({
+  command: "add",
+  describe: "Add a note.",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string",
+    },
+  },
+  handler: function (argv) {
+    console.log("Adding a note...");
+    console.log(argv);
+  },
+});
+
+yargs.command({
+  command: "remove",
+  describe: "Remove a note.",
+  handler: () => {
+    console.log("Removing a note...");
+  },
+});
+
+yargs.command({
+  command: "list",
+  describe: "List the notes.",
+  handler: () => {
+    console.log("Listing the notes...");
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe: "Read a note.",
+  handler: function () {
+    console.log("Reading a note...");
+  },
+});
+
+yargs.parse();
