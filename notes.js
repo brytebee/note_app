2bec529a34565861ad574a6c5251d8a04a964962
Atsighi Bright <brytebee@gmail.com>
2022-10-25 09:47:01 +0100

    :fire: Upfate note.js
    Re-order functions

--- notes.js
+++ notes.js
@@ -19,6 +19,8 @@ const saveNotes = (notes) => {
   fs.writeFileSync('notes.json', dataJSON);
 };
 
+const normalize = (input) => input.toLowerCase();
+
 const addNotes = function (title, body) {
   const notes = loadNotes();
   const duplicateNotes = notes.filter(
@@ -32,8 +34,6 @@ const addNotes = function (title, body) {
     console.log('Note title taken!');
   }
 };
-
-const normalize = (input) => input.toLowerCase();
 
 const removeNotes = (title) => {
   const notes = loadNotes();
